ee15b9f2f8bdc3489ca66971cf6542ec5a459b9e
jaionet <jaionet@br.ibm.com>
2024-06-12 00:31:08 -0400

    exercise #2

--- tests/factories.py
+++ tests/factories.py
@@ -22,6 +22,16 @@ from factory.fuzzy import FuzzyChoice, FuzzyDecimal
 from service.models import Product, Category
 
 
+# class ProductFactory(factory.Factory):
+#     """Creates fake products for testing"""
+
+#     class Meta:
+#         """Maps factory to data model"""
+
+#         model = Product
+
+#     id = factory.Sequence(lambda n: n)
+# Add code to create Fake Products
 class ProductFactory(factory.Factory):
     """Creates fake products for testing"""
 
@@ -31,4 +41,31 @@ class ProductFactory(factory.Factory):
         model = Product
 
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+    name = FuzzyChoice(
+        choices=[
+            "Hat",
+            "Pants",
+            "Shirt",
+            "Apple",
+            "Banana",
+            "Pots",
+            "Towels",
+            "Ford",
+            "Chevy",
+            "Hammer",
+            "Wrench"
+        ]
+    )
+    description = factory.Faker("text")
+    price = FuzzyDecimal(0.5, 2000.0, 2)
+    available = FuzzyChoice(choices=[True, False])
+    category = FuzzyChoice(
+        choices=[
+            Category.UNKNOWN,
+            Category.CLOTHS,
+            Category.FOOD,
+            Category.HOUSEWARES,
+            Category.AUTOMOTIVE,
+            Category.TOOLS,
+        ]
+    )
